d6c7f1c2724794c0205bc42c2a4e72f9234b6b39
Jordan Day <jordan@roboninja.com>
2015-06-26 21:23:57 -0500

    move notes

--- code/sieve.exs
+++ code/sieve.exs
@@ -4,11 +4,13 @@ defmodule Eratosthenes do
     IO.puts "Returning []\n"
     []
   end
+
   def sieve([current | rest]) when current < 2 do
     IO.puts "#{current} is less than 2, so it can't be prime."
     IO.puts "Returning sieve(#{inspect rest})\n"
     sieve(rest)
   end
+  
   def sieve([current | rest]) do
     filtered = Enum.filter(rest, fn num -> rem(num, current) != 0 end)
     IO.puts "#{current} is prime. Filtered out multiples of #{current}."
